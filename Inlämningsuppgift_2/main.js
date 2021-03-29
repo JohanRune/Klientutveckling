864f04b4117485df9af4467cfe07a8028711ac63
JohanRune <kjgrune@yahoo.se>
2021-03-29 16:42:39 +0200

    Utökad koll av form

--- Inlämningsuppgift_2/main.js
+++ Inlämningsuppgift_2/main.js
@@ -16,149 +16,106 @@ document.getElementById("toCart").addEventListener("click", goToCart);
 
 
 
-
 load();
 
-      function load() {
-        fetch("http://webacademy.se/fakestore/")
-          .then((response) => response.json())
-          .then((data) => render(data))
-          .catch((error) => console.error(error));
-      }
+function load() {
+  fetch("http://webacademy.se/fakestore/")
+    .then((response) => response.json())
+    .then((data) => render(data))
+    .catch((error) => console.error(error));
+}
 
-      function render(users) {
-        console.log(users);
+function render(products) {
+  console.log(products);
 
-        let compressed = JSON.stringify(users);
-        localStorage.setItem("jsondata", compressed);
-
-       // let uncompressed = JSON.parse(localStorage.getItem("jsondata"));
-        //console.log(uncompressed);
-    
-
-        let output = "";
-        for (let index = 1; index < 13; index++) {
-            output = 
-            "<hr> <h4>" + users[index].title + "</h3><br>" 
-            + users[index].description + "<br>"
-            + "<img src=" + users[index].image + " width=\"300\"> <br>"
-            + users[index].price + " euro <br>";
-            document.getElementById(index).insertAdjacentHTML('afterbegin', output);
-             
-    
-        }
-
-      }
+  let compressed = JSON.stringify(products);
+  localStorage.setItem("jsondata", compressed);
 
 
-      function goToCart(){
+
+  let output = "";
+  for (let index = 0; index < 12; index++) {
+      output = 
+      "<hr> <h4>" + products[index].title + "</h3><br>" 
+      + products[index].description + "<br>"
+      + "<img src=" + products[index].image + " width=\"300\"> <br>"
+      + products[index].price + " euro <br>";
+      document.getElementById(index).insertAdjacentHTML('afterbegin', output);
         
-        localStorage.setItem("basket", JSON.stringify(basket));
-        
-        const win = window.open("basket.html", '_blank');
 
-      }
+  }
 
-      var basket = [];
+}
+
+
+function goToCart(){
+  
+  localStorage.setItem("basket", JSON.stringify(basket));
+  
+  const win = window.open("basket.html", '_blank');
+
+}
+
+var basket = [];
 
      
 
 function saveToBasket1(e){ 
-  
-  products = JSON.parse(localStorage.getItem("jsondata"));
-  
-
   basket.push("0");
-  for (let index = 0; index < basket.length; index++) {
-    console.log(basket[index])
-  }
-
-
-//localStorage.setItem("basket", basket)
-
-
-let compressed = JSON.stringify(products[0]); //ändra siffran till vilken knapp som har använts.
-  localStorage.setItem("choosenProduct", compressed);
-
-
-//G-nivå
-/*   users = JSON.parse(localStorage.getItem("jsondata"));
-  let compressed = JSON.stringify(users[0]); //ändra siffran till vilken knapp som har använts.
-  localStorage.setItem("choosenProduct", compressed);
-  console.log(compressed)
- */
- // const win = window.open("basket.html", '_blank');
-  //win.focus();
   e.preventDefault();
 }
 
-
-function saveToBasket2(e){ //sätt siffra efter utifrån vilken knapp som tryckts. 
+function saveToBasket2(e){ 
   basket.push("1");
   e.preventDefault();
 }
-//localStorage.setItem("basket", basket)
 
+function saveToBasket3(e){  
+  basket.push("2");
+  e.preventDefault();
+}
 
-//let compressed = JSON.stringify(products[1]); //ändra siffran till vilken knapp som har använts.
- // localStorage.setItem("choosenProduct", compressed);
+function saveToBasket4(e){  
+  basket.push("3");
+  e.preventDefault();
+}
 
+function saveToBasket5(e){  
+  basket.push("4");
+  e.preventDefault();
+}
 
-//G-nivå
-/*   users = JSON.parse(localStorage.getItem("jsondata"));
-  let compressed = JSON.stringify(users[0]); //ändra siffran till vilken knapp som har använts.
-  localStorage.setItem("choosenProduct", compressed);
-  console.log(compressed)
- */
- // const win = window.open("basket.html", '_blank');
-  //win.focus();
-  
-  function saveToBasket3(e){ //sätt siffra efter utifrån vilken knapp som tryckts. 
-    basket.push("2");
-    e.preventDefault();
-  }
+function saveToBasket6(e){
+  basket.push("5");
+  e.preventDefault();
+}
 
-  function saveToBasket4(e){ //sätt siffra efter utifrån vilken knapp som tryckts. 
-    basket.push("3");
-    e.preventDefault();
-  }
+function saveToBasket7(e){ 
+  basket.push("6");
+  e.preventDefault();
+}
 
-  function saveToBasket5(e){ //sätt siffra efter utifrån vilken knapp som tryckts. 
-    basket.push("4");
-    e.preventDefault();
-  }
+function saveToBasket8(e){  
+  basket.push("7");
+  e.preventDefault();
+}
 
-  function saveToBasket6(e){ //sätt siffra efter utifrån vilken knapp som tryckts. 
-    basket.push("5");
-    e.preventDefault();
-  }
+function saveToBasket9(e){ 
+  basket.push("8");
+  e.preventDefault();
+}
 
-  function saveToBasket7(e){ //sätt siffra efter utifrån vilken knapp som tryckts. 
-    basket.push("6");
-    e.preventDefault();
-  }
+function saveToBasket10(e){ 
+  basket.push("9");
+  e.preventDefault();
+}
 
-  function saveToBasket8(e){ //sätt siffra efter utifrån vilken knapp som tryckts. 
-    basket.push("7");
-    e.preventDefault();
-  }
+function saveToBasket11(e){ 
+  basket.push("10");
+  e.preventDefault();
+}
 
-  function saveToBasket9(e){ //sätt siffra efter utifrån vilken knapp som tryckts. 
-    basket.push("8");
-    e.preventDefault();
-  }
-
-  function saveToBasket10(e){ //sätt siffra efter utifrån vilken knapp som tryckts. 
-    basket.push("9");
-    e.preventDefault();
-  }
-
-  function saveToBasket11(e){ //sätt siffra efter utifrån vilken knapp som tryckts. 
-    basket.push("10");
-    e.preventDefault();
-  }
-
-  function saveToBasket12(e){ //sätt siffra efter utifrån vilken knapp som tryckts. 
-    basket.push("11");
-    e.preventDefault();
-  }
+function saveToBasket12(e){
+  basket.push("11");
+  e.preventDefault();
+}
--- Inlämningsuppgift_2/main2.js
+++ Inlämningsuppgift_2/main2.js
@@ -1,55 +1,26 @@
 
-// G-nivå
-    /*  let uncompressed = JSON.parse(localStorage.getItem("choosenProduct"));
-     console.log(uncompressed.description);
-     //document.getElementById("product").innerHTML = uncompressed.description;
-     document.getElementById("1").style.visibility="hidden";
- */
-/*     output = 
-    "<hr> <h4>" + uncompressed.title + "</h3><br>" 
-    + uncompressed.description + "<br>"
-    + "<img src=" + uncompressed.image + " width=\"300\"> <br>"
-    + uncompressed.price + "<br>";
-    document.getElementById("product").innerHTML = output;
- */
-  
-   // document.getElementById("product").innerHTML=choosenProduct;
-
-
-//VG-nivå
-
-//1. ladda ner alla produkter.
-//1.5 Ladda ner arrayen med valda produkter. 
-//2. Ladda upp info om valda produkter. 
-
 let products = JSON.parse(localStorage.getItem("jsondata"));
- // console.log(products);
 let arrayBoughtProducts = JSON.parse(localStorage.getItem("basket"));
-//console.log(arrayBoughtProducts[1])
+
 for (let index = 0; index < arrayBoughtProducts.length; index++) {
     console.log(arrayBoughtProducts[index])
   }
 
-  //document.getElementById("row").style.visibility="hidden";
-
     
-    let output = "";
-    for (let index = 0; index < arrayBoughtProducts.length; index++) {
-        let theproduct = arrayBoughtProducts[index];
-        output = 
-        "<hr> <h6>" + products[theproduct].title + "</h6><br>" 
-        //+ products[theproduct].description + "<br>"
-        //+ "<img src=" + products[theproduct].image + " width=\"300\"> <br>"
-        + products[theproduct].price + " euro <br>"
-        + "Antal:";
+let output = "";
+for (let index = 0; index < arrayBoughtProducts.length; index++) {
+    let theproduct = arrayBoughtProducts[index]; //valda produkter plockas fram ur arrayen
+    output = 
+    "<hr> <h6>" + products[theproduct].title + "</h6><br>" 
+    + products[theproduct].price + " euro <br>"
+    + "Antal: <br>"
 
-       // document.getElementById(index).style.visibility="visible";
-        document.getElementById(index).insertAdjacentHTML('afterbegin', output);
+    document.getElementById(index).insertAdjacentHTML('afterbegin', output);
 }
 
 
-
 if (arrayBoughtProducts.length == 1){
+    document.getElementById("button1").style.display="none";
     document.getElementById("button2").style.display="none";
     document.getElementById("button3").style.display="none";
     document.getElementById("button4").style.display="none";
@@ -58,10 +29,10 @@ if (arrayBoughtProducts.length == 1){
     document.getElementById("button7").style.display="none";
     document.getElementById("button8").style.display="none";
     document.getElementById("button9").style.display="none";
-    document.getElementById("button10").style.display="none";
 }
 
 else if (arrayBoughtProducts.length == 2){
+    document.getElementById("button2").style.display="none";
     document.getElementById("button3").style.display="none";
     document.getElementById("button4").style.display="none";
     document.getElementById("button5").style.display="none";
@@ -69,65 +40,88 @@ else if (arrayBoughtProducts.length == 2){
     document.getElementById("button7").style.display="none";
     document.getElementById("button8").style.display="none";
     document.getElementById("button9").style.display="none";
-    document.getElementById("button10").style.display="none";
 }
 
 else if (arrayBoughtProducts.length == 3){
+    document.getElementById("button3").style.display="none";
     document.getElementById("button4").style.display="none";
     document.getElementById("button5").style.display="none";
     document.getElementById("button6").style.display="none";
     document.getElementById("button7").style.display="none";
     document.getElementById("button8").style.display="none";
     document.getElementById("button9").style.display="none";
-    document.getElementById("button10").style.display="none";
 }
 
 else if (arrayBoughtProducts.length == 4){
+    document.getElementById("button4").style.display="none";
     document.getElementById("button5").style.display="none";
     document.getElementById("button6").style.display="none";
     document.getElementById("button7").style.display="none";
     document.getElementById("button8").style.display="none";
     document.getElementById("button9").style.display="none";
-    document.getElementById("button10").style.display="none";
 }
 
 else if (arrayBoughtProducts.length == 5){
+    document.getElementById("button5").style.display="none";
     document.getElementById("button6").style.display="none";
     document.getElementById("button7").style.display="none";
     document.getElementById("button8").style.display="none";
     document.getElementById("button9").style.display="none";
-    document.getElementById("button10").style.display="none";
 }
 
 else if (arrayBoughtProducts.length == 6){
+    document.getElementById("button6").style.display="none";
     document.getElementById("button7").style.display="none";
     document.getElementById("button8").style.display="none";
     document.getElementById("button9").style.display="none";
-    document.getElementById("button10").style.display="none";
 }
 
 else if (arrayBoughtProducts.length == 7){
+    document.getElementById("button7").style.display="none";
     document.getElementById("button8").style.display="none";
     document.getElementById("button9").style.display="none";
-    document.getElementById("button10").style.display="none";
 }
 
 else if (arrayBoughtProducts.length == 8){
+    document.getElementById("button8").style.display="none";
     document.getElementById("button9").style.display="none";
-    document.getElementById("button10").style.display="none";
 }
 
 else if (arrayBoughtProducts.length == 9){
-    document.getElementById("button10").style.display="none";
+    document.getElementById("button9").style.display="none";
 }
 
+let totalPrice = 0;
 
+totalPriceCalculation();
+
+function totalPriceCalculation () {
+    
+    for (let index = 0; index < arrayBoughtProducts.length; index++) {
+        let theproduct = arrayBoughtProducts[index];
+        totalPrice += (document.getElementById(`button${index}`).value * products[theproduct].price);
+        }
+}
+
+console.log(totalPrice);
+
+let twoDecimals = 0;
+twoDecimals = totalPrice.toFixed(2);
+
+document.getElementById("totalPrice").innerHTML= twoDecimals;
+
+
+document.getElementById("update").addEventListener("click", updateBasket);
+
+function updateBasket(){
+    totalPrice = 0;
+    totalPriceCalculation();
+    document.getElementById("totalPrice").innerHTML= totalPrice;
+}
 
 
 
     localStorage.clear();
-
-
 
 
 function validateForm() {
@@ -137,9 +131,19 @@ function validateForm() {
         return false;
     }
 
+    if (!/^[a-zA-Z]*$/g.test(x)){
+        alert ("Letters only in name");
+        return false;
+    }
+
     var x = document.forms["myForm"]["telephone"].value;
     if (x == "") {
         alert ("Telephone number must be filled out");
+        return false;
+    }
+
+    if(isNaN(x)){
+        alert ("Not a number in telephone");
         return false;
     }
 
@@ -148,10 +152,18 @@ function validateForm() {
         alert ("Email address must be filled out");
         return false;
     }
+    if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/g.test(x)){
+        alert ("Not a valid email address");
+        return false;
+    }
 
     var x = document.forms["myForm"]["street"].value;
     if (x == "") {
         alert ("Street address must be filled out");
+        return false;
+    }
+    if (!/^[a-zA-Z0-9]*$/g.test(x)){
+        alert ("Only letters and numbers in street address");
         return false;
     }
 
@@ -160,4 +172,12 @@ function validateForm() {
         alert ("Postal code and city must be filled out");
         return false;
     }
+    if (!/^[a-zA-Z0-9]*$/g.test(x)){
+        alert ("Only letters and numbers in postal number and city");
+        return false;
+    }
+
 }
+
+
+
